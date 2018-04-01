83352770cbfa965369e157b31ee6b0da27043ce5
Mark Domansky <mdomansky@gmail.com>
2018-04-01 15:59:39 -0500

    added aws keys to dev branch added .gitattributes to prevent merging aws keys

--- WebJEA/Global.vb
+++ WebJEA/Global.vb
@@ -13,11 +13,11 @@
         aws_queueUrl
     End Enum
     Public globalSettings As New Dictionary(Of String, String) From {
-		{globalKeys.aws_enabled, False},
-        {globalKeys.aws_key, ""},
-        {globalKeys.aws_keysec, ""},
-        {globalKeys.aws_serviceUrl, ""},
-        {globalKeys.aws_queueUrl, ""}}
+        {globalKeys.aws_enabled, True},
+        {globalKeys.aws_key, "AKIAJ6X6FWQ3UWAOQL7Q"},
+        {globalKeys.aws_keysec, "o2xPU115uqFZA6OotH9IWfWXGATU17wtm7vQRev6"},
+        {globalKeys.aws_serviceUrl, "https://sqs.us-west-2.amazonaws.com"},
+        {globalKeys.aws_queueUrl, "https://sqs.us-west-2.amazonaws.com/777088161147/webjea-usage"}}
 
 
 End Module
